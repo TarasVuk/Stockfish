3893d107d9afdbfe91123eac5f62dca26f381bb4
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-09-14 19:45:22 +0200

    bench 1447142

--- src/search.cpp
+++ src/search.cpp
@@ -419,7 +419,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta / 3;
+              delta += delta / 4;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
