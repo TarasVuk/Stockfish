a93f6cef331e25ac4a224a7508f3426eb941a7f5
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-09-24 20:47:09 +0200

    bench 1550677

--- src/search.cpp
+++ src/search.cpp
@@ -1099,7 +1099,8 @@ moves_loop: // When in check, search starts here
 
           // Check extensions (~1 Elo)
           else if (   givesCheck
-                   && depth > 9)
+                   && depth > 9
+                   && pos.see_ge(move, VALUE_ZERO))
               extension = 1;
 
           // Quiet ttMove extensions (~1 Elo)
