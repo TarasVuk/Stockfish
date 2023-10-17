d65a71e2a9a1ebf1c95df85c25d1c070b31fdf13
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-10-17 16:27:59 +0200

    bench 1059819

--- src/search.cpp
+++ src/search.cpp
@@ -1565,7 +1565,7 @@ moves_loop: // When in check, search starts here
 
                 // If static eval is much lower than alpha and move is not winning material
                 // we can prune this move
-                if (futilityBase <= alpha && !pos.see_ge(move, VALUE_ZERO + 1))
+                if (futilityBase <= alpha && !pos.see_ge(move, (alpha - futilityBase) / 2))
                 {
                     bestValue = std::max(bestValue, futilityBase);
                     continue;
