8e50ea8dcd6b0f19ede98c861261c37ad6f53233
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-10-26 15:44:05 +0200

    bench 1244281

--- src/search.cpp
+++ src/search.cpp
@@ -840,6 +840,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     if (
       !PvNode && depth > 3
       && abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
+      && (!ttMove || ttCapture)
       // If value from transposition table is lower than probCutBeta, don't attempt probCut
       // there and in further interactions with transposition table cutoff depth is set to depth - 3
       // because probCut search has depth set to depth - 4 but we also do a move before it
