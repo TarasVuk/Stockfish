780f9d6e05ae2315b640e74ae43987864e711ec9
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-11-06 18:48:06 +0100

    bench 1274650

--- src/search.cpp
+++ src/search.cpp
@@ -785,7 +785,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != MOVE_NULL && (ss - 1)->statScore < 17257 && eval >= beta
         && eval >= ss->staticEval && ss->staticEval >= beta - 24 * depth + 281 && !excludedMove
-        && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
+        && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly && !ttCapture
         && beta > VALUE_TB_LOSS_IN_MAX_PLY)
     {
         assert(eval - beta >= 0);
