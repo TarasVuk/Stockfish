82f02f62204dfe76dc48b8f62de48e12bf352595
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-11-08 17:49:46 +0100

    bench 1327013

--- src/search.cpp
+++ src/search.cpp
@@ -90,7 +90,7 @@ Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta) {
 }
 
 constexpr int futility_move_count(bool improving, Depth depth) {
-    return improving ? (3 + depth * depth) : (3 + depth * depth) / 3;
+    return improving ? (3 + depth * depth) : (3 + depth * depth / 2);
 }
 
 // History and stats update bonus, based on depth
