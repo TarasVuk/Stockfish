3d74837410ff066de25526ed9c36c17e5c3e450d
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-11-10 18:25:28 +0100

    bench 1226352

--- src/search.cpp
+++ src/search.cpp
@@ -1127,7 +1127,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction for cut nodes (~3 Elo)
         if (cutNode)
-            r += 2;
+            r += 2 + !ttMove;
 
         // Increase reduction if ttMove is a capture (~3 Elo)
         if (ttCapture)
