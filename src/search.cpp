9f54ccc32ec01d08117650191b18b3aec1f53c04
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-11-15 19:56:14 +0100

    bench 1267601

--- src/search.cpp
+++ src/search.cpp
@@ -1093,7 +1093,7 @@ moves_loop:  // When in check, search starts here
             }
 
             // Check extensions (~1 Elo)
-            else if (givesCheck && depth > 9)
+            else if ((PvNode || cutNode) && givesCheck && depth > 9)
                 extension = 1;
 
             // Quiet ttMove extensions (~1 Elo)
