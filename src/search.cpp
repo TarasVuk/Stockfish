e65ab1056959a8e383f3a49f77787d1745c1f2e8
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-11-19 09:36:38 +0100

    bench 1643255

--- src/search.cpp
+++ src/search.cpp
@@ -1098,7 +1098,8 @@ moves_loop:  // When in check, search starts here
 
             // Quiet ttMove extensions (~1 Elo)
             else if (PvNode && move == ttMove && move == ss->killers[0]
-                     && (*contHist[0])[movedPiece][to_sq(move)] >= 4194)
+                     && (*contHist[0])[movedPiece][to_sq(move)] > 4193
+                     && thisThread->mainHistory[us][from_to(move)] > 2000)
                 extension = 1;
 
             // Recapture extensions (~1 Elo)
