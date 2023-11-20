68b4f3ea428c413aac58a38a33b317fa569a5038
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-11-20 18:25:25 +0100

    bench 1471694

--- src/search.cpp
+++ src/search.cpp
@@ -1336,7 +1336,7 @@ moves_loop:  // When in check, search starts here
     assert(moveCount || !ss->inCheck || excludedMove || !MoveList<LEGAL>(pos).size());
 
     if (!moveCount)
-        bestValue = excludedMove ? alpha : ss->inCheck ? mated_in(ss->ply) : VALUE_DRAW;
+        bestValue = excludedMove ? -VALUE_INFINITE : ss->inCheck ? mated_in(ss->ply) : VALUE_DRAW;
 
     // If there is a move that produces search value greater than alpha we update the stats of searched moves
     else if (bestMove)
