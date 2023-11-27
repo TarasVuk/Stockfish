d771edb9d32c6ad1f846377cd51a551c1a777e9a
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-11-27 15:41:52 +0100

    bench 1457524

--- src/search.cpp
+++ src/search.cpp
@@ -1465,13 +1465,7 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
 
         // Stand pat. Return immediately if static value is at least beta
         if (bestValue >= beta)
-        {
-            if (!ss->ttHit)
-                tte->save(posKey, value_to_tt(bestValue, ss->ply), false, BOUND_LOWER, DEPTH_NONE,
-                          MOVE_NONE, ss->staticEval);
-
             return bestValue;
-        }
 
         if (bestValue > alpha)
             alpha = bestValue;
