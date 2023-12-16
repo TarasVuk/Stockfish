74c8de901941c9605a819c07091a471d5d597a3a
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2023-12-16 12:19:17 +0100

    bench 1247072

--- src/search.cpp
+++ src/search.cpp
@@ -823,6 +823,8 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
             if (v >= beta)
                 return nullValue;
         }
+        else if (depth > 1)
+            depth--;
     }
 
     // Step 10. Internal iterative reductions (~9 Elo)
