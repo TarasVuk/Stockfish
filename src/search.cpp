4cd1c1dcda7229669162e156ae612679758b879c
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2024-01-17 16:04:41 +0100

    bench 1135433

--- src/search.cpp
+++ src/search.cpp
@@ -1182,6 +1182,9 @@ moves_loop:  // When in check, search starts here
         // Increase reduction on repetition (~1 Elo)
         if (move == (ss - 4)->currentMove && pos.has_repeated())
             r += 2;
+        
+        if (std::abs(thisThread->correctionHistory[us][pawn_structure_index<Correction>(pos)]) < 128)
+            r++;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
