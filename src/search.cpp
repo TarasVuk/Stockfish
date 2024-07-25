b2c334a072de5f9e0057fa117e24eb5369367b32
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2024-07-25 20:15:25 +0200

    bench 1164613

--- src/search.cpp
+++ src/search.cpp
@@ -808,12 +808,12 @@ Value Search::Worker::search(
 
     // Step 10. Internal iterative reductions (~9 Elo)
     // For PV nodes without a ttMove, we decrease depth.
-    if (PvNode && !ttData.move)
+    if (ss->ttPv && !ttData.move)
         depth -= 3;
 
     // Use qsearch if depth <= 0
     if (depth <= 0)
-        return qsearch<PV>(pos, ss, alpha, beta);
+        return qsearch<PvNode ? PV : NonPV>(pos, ss, alpha, beta);
 
     // For cutNodes, if depth is high enough, decrease depth by 2 if there is no ttMove,
     // or by 1 if there is a ttMove with an upper bound.
