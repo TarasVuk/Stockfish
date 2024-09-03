3ed252cc009e755ce6154c67b76666d0357e4422
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2024-09-03 17:45:41 +0200

    bench 1520404

--- src/search.cpp
+++ src/search.cpp
@@ -351,6 +351,7 @@ void Search::Worker::iterative_deepening() {
                 }
                 else if (bestValue >= beta)
                 {
+                    alpha  = (alpha + beta) / 2;
                     beta = std::min(bestValue + delta, VALUE_INFINITE);
                     ++failedHighCnt;
                 }
