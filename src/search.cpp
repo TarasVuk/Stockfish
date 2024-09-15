9e383badac599f382744b75b1aa29ceaa3773775
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2024-09-15 21:30:13 +0200

    bench 1393309

--- src/search.cpp
+++ src/search.cpp
@@ -1390,7 +1390,7 @@ moves_loop:  // When in check, search starts here
         && !(bestValue >= beta && bestValue <= ss->staticEval)
         && !(!bestMove && bestValue >= ss->staticEval))
     {
-        auto bonus = std::clamp(int(bestValue - ss->staticEval) * depth / 8,
+        auto bonus = std::clamp(int(bestValue - ss->staticEval) * std::min(depth, 8) / 8,
                                 -CORRECTION_HISTORY_LIMIT / 4, CORRECTION_HISTORY_LIMIT / 4);
         thisThread->pawnCorrectionHistory[us][pawn_structure_index<Correction>(pos)] << bonus;
         thisThread->materialCorrectionHistory[us][material_index(pos)] << bonus;
