0af7f5b3b611b98806ae3677f9e098b294861f94
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2024-09-26 21:38:03 +0200

    bench 1457787

--- src/search.cpp
+++ src/search.cpp
@@ -842,8 +842,8 @@ Value Search::Worker::search(
     if (cutNode && depth >= 7 && (!ttData.move || ttData.bound == BOUND_UPPER))
         depth -= 1 + !ttData.move;
 
-    if (PvNode && ttData.move && ttData.depth > depth + 2 && depth < MAX_PLY - 1)
-        depth++;
+    if (PvNode && ttData.move && ttData.depth > depth * 2 && depth < MAX_PLY - 1)
+        depth += 1 + (depth > 8);
 
     // Step 11. ProbCut (~10 Elo)
     // If we have a good enough capture (or queen promotion) and a reduced search
