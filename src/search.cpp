a56f7eb2867e8141a72397fa0e2b4876a24e98a0
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2024-10-16 21:10:14 +0200

    bench 1327516

--- src/search.cpp
+++ src/search.cpp
@@ -821,7 +821,7 @@ Value Search::Worker::search(
         if (nullValue >= beta && nullValue < VALUE_TB_WIN_IN_MAX_PLY)
         {
             if (thisThread->nmpMinPly || depth < 16)
-                return nullValue;
+                return beta + (nullValue - beta) / 3;
 
             assert(!thisThread->nmpMinPly);  // Recursive verification is not allowed
 
@@ -834,7 +834,7 @@ Value Search::Worker::search(
             thisThread->nmpMinPly = 0;
 
             if (v >= beta)
-                return nullValue;
+                return beta + (nullValue - beta) / 3;
         }
     }
 
