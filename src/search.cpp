69bb3da002e0cdada236f7b405020066cb28c3f2
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2024-10-24 18:04:55 +0200

    bench 1283457

--- src/search.cpp
+++ src/search.cpp
@@ -455,7 +455,7 @@ void Search::Worker::iterative_deepening() {
             double reduction = (1.48 + mainThread->previousTimeReduction) / (2.17 * timeReduction);
             double bestMoveInstability = 1 + 1.88 * totBestMoveChanges / threads.size();
             double recapture           = limits.capSq == rootMoves[0].pv[0].to_sq() ? 0.955 : 1.005;
-            double subtree             = nodesEffort >= 75 ? 0.990 : 1.010;
+            double subtree             = nodesEffort >= 50 ? 0.990 : 1.010;
 
             double totalTime =
               mainThread->tm.optimum() * fallingEval * reduction * bestMoveInstability * recapture * subtree;
