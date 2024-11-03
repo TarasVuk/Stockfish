2af47405d164b04c9b3cdca66201bb6787b074be
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2024-11-03 14:13:15 +0100

    bench 1052550

--- src/search.cpp
+++ src/search.cpp
@@ -1175,7 +1175,7 @@ moves_loop:  // When in check, search starts here
 
         // For first picked move (ttMove) reduce reduction (~3 Elo)
         else if (moveCount == 1)
-            r -= 1 + (move == ttData.move);
+            r -= 2;
 
         ss->statScore = 2 * thisThread->mainHistory[us][move.from_to()]
                       + (*contHist[0])[movedPiece][move.to_sq()]
