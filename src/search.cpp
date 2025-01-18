889f0ad7f3dd2917c36569f56e9df7b54a949481
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2025-01-18 13:12:54 +0100

    bench 1243415

--- src/search.cpp
+++ src/search.cpp
@@ -794,7 +794,7 @@ Value Search::Worker::search(
     // Step 9. Null move search with verification search (~35 Elo)
     if (cutNode && (ss - 1)->currentMove != Move::null() && eval >= beta
         && ss->staticEval >= beta - 20 * depth + 440 && !excludedMove && pos.non_pawn_material(us)
-        && ss->ply >= thisThread->nmpMinPly && !is_loss(beta))
+         && pos.non_pawn_material(~us) && ss->ply >= thisThread->nmpMinPly && !is_loss(beta))
     {
         assert(eval - beta >= 0);
 
