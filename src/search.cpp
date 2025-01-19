28b520fc848154fe685264457721e78af3351965
Taras Vuk <117687515+TarasVuk@users.noreply.github.com>
2025-01-19 18:07:50 +0100

    bench 1620786

--- src/search.cpp
+++ src/search.cpp
@@ -800,7 +800,7 @@ Value Search::Worker::search(
         return beta + (eval - beta) / 3;
 
     improving |= ss->staticEval >= beta + 97;
-    improving |= correctionValue / 131072 >= 50;
+    improving |= correctionValue / 131072 >= 75;
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (cutNode && (ss - 1)->currentMove != Move::null() && eval >= beta
